811d59d6edfa1be9d167a7d4ecf24d3ca8317c5a
DhruvDua1105 <dhruv.dua.ug20@nsut.ac.in>
2024-02-11 18:25:33 +0530

    token validity extended

--- main.py
+++ main.py
@@ -195,7 +195,7 @@ def create_access_token(data: dict, expires_delta: timedelta = None):
     if expires_delta:
         expire = datetime.utcnow() + expires_delta
     else:
-        expire = datetime.utcnow() + timedelta(minutes=100)
+        expire = datetime.utcnow() + timedelta(days=80)
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
